93cb5bb8867a930abbade0ef042ff15759ad55e1
elery2711 <elery2711@gmail.com>
2024-12-09 20:35:24 -0700

    Refactor server structure by removing package.json and package-lock.json, and adding routes for API endpoints

--- server/app.js
+++ server/app.js
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const routes = require('./routes/index.routes');
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use('/api', routes);
 
 module.exports = app;
